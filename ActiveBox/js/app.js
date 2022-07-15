4ac450c9dbd4c49145cb073cbbd121409037c49d
gerl0n <gerl0n@yandex.ru>
2022-07-15 03:20:38 +0300

    Changes to be committed: 	modified:   ActiveBox/css/style.css 	modified:   ActiveBox/index.html 	modified:   ActiveBox/js/app.js

--- ActiveBox/js/app.js
+++ ActiveBox/js/app.js
@@ -1,53 +1,61 @@
-$(function() {
+$(function () {
+  let header = $(".JSheader");
+  let intro = $(".JSintro");
+  let introH = intro.innerHeight();
+  let ScrollPos = $(window).scrollTop();
 
-    let header = $(".JSheader");
-    let intro = $(".JSintro");
-    let introH = intro.innerHeight();;
-    let ScrollPos = $(window).scrollTop();
+  checkScroll(ScrollPos, introH);
 
-    checkScroll (ScrollPos, introH)
+  // fixed header
+  $(window).on("scroll resize", function () {
+    introH = intro.innerHeight();
+    ScrollPos = $(this).scrollTop();
+    checkScroll(ScrollPos, introH);
+  });
 
-    // fixed header
-    $(window).on("scroll resize", function() {
-        introH = intro.innerHeight();
-        ScrollPos = $(this).scrollTop();
-        checkScroll(ScrollPos, introH)
-    });
+  function checkScroll(ScrollPos, introH) {
+    if (ScrollPos > introH-67) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
 
-    function checkScroll (ScrollPos, introH){
-        if( ScrollPos > introH ) {
-            header.addClass("fixed");
-        } else {
-            header.removeClass("fixed");
-        }
-    };
+  // navToggle
+  navToggle = $(".jsToggle");
+  nav = $(".jsNav");
 
-    // navToggle
-    navToggle = $('.jsToggle')
-    nav = $('.jsNav')
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+    nav.toggleClass("show");
+  });
 
-    navToggle.on("click", function(event){
-        event.preventDefault();
-        nav.toggleClass("show");
-    });
+  // smooth scroll
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
 
-    // smooth scroll
-    $("[data-scroll]").on("click", function(event) {
-        event.preventDefault();
+    let ElemID = $(this).data("scroll");
+    let ElemOffset = $(ElemID).offset().top;
 
-        let ElemID = $(this).data('scroll');
-        let ElemOffset = $(ElemID).offset().top;
+    nav.removeClass("show");
 
-        nav.removeClass("show");
+    $("html, body").animate(
+      {
+        scrollTop: ElemOffset - 66,
+      },
+      700
+    );
+  });
 
-        $('html, body').animate({
-            scrollTop: ElemOffset - 70
-        }, 700);
-    });
+  // slider https://kenwheeler.github.io/slick/
+  let review_slider = $(".review_slider");
 
-
-
-
-
-
+  review_slider.slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1, 
+    fade: true,
+    arrows: false,
+    dots:true
+  });
 });
